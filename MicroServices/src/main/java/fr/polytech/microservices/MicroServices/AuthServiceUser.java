e800153eb21efc5e765f958af55ba6aa75860781
Théo Michels <theomichels@hotmail.fr>
2021-11-16 10:48:07 +0100

    add all

---
+++ MicroServices/src/main/java/fr/polytech/microservices/MicroServices/AuthServiceUser.java
@@ -0,0 +1,28 @@
+package fr.polytech.microservices.MicroServices;
+
+public class AuthServiceUser {
+
+    private Long id;
+    private String password;
+
+    public AuthServiceUser(Long id) {
+        super();
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- MicroServices/src/main/java/fr/polytech/microservices/MicroServices/ProfileController.java
+++ MicroServices/src/main/java/fr/polytech/microservices/MicroServices/ProfileController.java
@@ -1,17 +1,19 @@
 package fr.polytech.microservices.MicroServices;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProfileController {
@@ -20,6 +22,9 @@ public class ProfileController {
 	private final Map<Long, Profile> profiles = new HashMap<>();
 	private final Set<String> emails = new HashSet<>();
 	private Logger logger = LoggerFactory.getLogger(ProfileController.class);
+
+	@Value("$service.authentication")
+	private String auth_service_url;
 	
 	@GetMapping("/PS/profiles")
 	@CrossOrigin
@@ -29,7 +34,7 @@ public class ProfileController {
 
 	@GetMapping("/PS/profiles/{id}/name")
 	@CrossOrigin
-	public String profile_get_name(@PathVariable(value = "id") Long id){
+	public String profile_get_name(@PathVariable(value = "id") Long id) {
 		logger.trace("GET /PS/profiles");
 		if(!profiles.containsKey(id)) {
 			throw new ProfileNotFoundException(id);
@@ -56,6 +61,16 @@ public class ProfileController {
 		String email = profile.getEmail();
 		long new_id = counter.incrementAndGet();
 		profile.setId(new_id);
+
+		AuthServiceUser auth_service_user = new AuthServiceUser(new_id);
+		RestTemplate restTemplate = new RestTemplate();
+		Long check_id = restTemplate.postForObject(
+				auth_service_url+"/AS/users",
+				auth_service_user,
+				Long.class);
+
+		if(!check_id.equals(new_id)) throw new RuntimeException();
+
 		profiles.put(new_id, profile);
 		emails.add(email);
 		logger.info(String.format("Profile created: [%d] %s.",
@@ -65,8 +80,18 @@ public class ProfileController {
 	
 	@DeleteMapping("/PS/profiles/{id}")
 	@CrossOrigin
-	public void profiles_delete(@PathVariable(value = "id") Long id) {
+	public void profiles_delete(@PathVariable(value = "id") Long id, @RequestHeader(value = "X-token") String token) {
 		logger.trace(String.format("DELETE /PS/profiles/%d", id));
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("X-Token", token);
+		HttpEntity<String> entity = new HttpEntity<String>("", headers);
+
+		ResponseEntity<Long> response = restTemplate.exchange(auth_service_url+"/AS/token", HttpMethod.GET, entity, Long.class);
+		Long token_user = response.getBody();
+		if(!Objects.equals(token_user, id)) throw new RuntimeException();
+
 		Profile profile = profiles.get(id);
 		emails.remove(profile.getEmail());
 		logger.trace(String.format("Deleted profile [%d] %s.", profile.getId(), profile.getName()));
