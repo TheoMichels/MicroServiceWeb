554a9de0320e96459af2eb46868b14d2c6d64f0d
Théo Michels <theomichels@hotmail.fr>
2021-11-30 08:47:29 +0100

    Add gateway method

--- Gateway/src/main/java/fr/polytech/gateway/demo/GatewayApplication.java
+++ Gateway/src/main/java/fr/polytech/gateway/demo/GatewayApplication.java
@@ -1,12 +1,32 @@
 package fr.polytech.gateway.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Value("${service.authentication}")
+	private String auth_service_url;
+
+	@Value("${service.profile}")
+	private String profile_service_url;
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/profiles")
+						.uri(profile_service_url+"/PS"))
+				.build();
 	}
 }
--- authentifDemo/src/main/java/fr/polytech/authentification/authentifDemo/AuthentifDemoApplication.java
+++ authentifDemo/src/main/java/fr/polytech/authentification/authentifDemo/AuthentifDemoApplication.java
@@ -1,7 +1,5 @@
 package fr.polytech.authentification.authentifDemo;
 
-import fr.polytech.authentification.authentifDemo.Controller.UserController;
-import fr.polytech.authentification.authentifDemo.Model.Token;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
